e1bf2bb7268647818152c741496c62b23f2df82d
ssook <cesces333@naver.com>
2021-09-13 00:04:30 +0900

    [programmers]43165 fix

--- programmers/43165.py
+++ programmers/43165.py
@@ -1,15 +1,16 @@
 # 출처 : https://programmers.co.kr/learn/courses/30/lessons/43165
 def solution(numbers, target):
-    answer = 0
+    answer = []
 
     def dfs(level, sum, answer):
         if(level == len(numbers)):
             if(sum == target):
-                answer += 1
-            return answer
+                answer.append(1)
+            return
+
         dfs(level+1, sum+numbers[level], answer)
         dfs(level+1, sum-numbers[level], answer)
 
-    answer = dfs(0, 0, answer)
+    dfs(0, 0, answer)
 
-    return answer
+    return len(answer)
