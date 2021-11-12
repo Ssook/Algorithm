dabc071302f5876d898a08c7db28994926069bcc
ssook <cesces333@naver.com>
2021-11-12 17:37:17 +0900

    [boj]1181 개선

--- boj/1181.py
+++ boj/1181.py
@@ -1,38 +1,46 @@
-# 출처 : https://www.acmicpc.net/problem/1181
-from collections import deque
+# # 출처 : https://www.acmicpc.net/problem/1181
+# from collections import deque
+# n = int(input())
+# words = []
+# for i in range(n):
+#     word = input()
+#     if(word not in words):
+#         words.append(word)
+
+# words.sort(key=lambda x: (len(x)))
+
+# queue = deque(words)
+# # print(words)
+# splitWords = []
+# w = len(words[0])
+# stack = []
+# while(queue):
+#     cur = queue.popleft()
+#     if(len(cur) == w):
+#         stack.append(cur)
+#         w = len(cur)
+#     else:
+#         splitWords.append(stack)
+#         stack = []
+#         stack.append(cur)
+#         w = len(cur)
+
+# # print(stack)
+# if(stack):
+#     splitWords.append(stack)
+# # print(splitWords)
+
+# for i in range(len(splitWords)):
+#     splitWords[i].sort()
+
+# for i in range(len(splitWords)):
+#     for j in range(len(splitWords[i])):
+#         print(splitWords[i][j])
+
+
 n = int(input())
-words = []
-for i in range(n):
-    word = input()
-    if(word not in words):
-        words.append(word)
-
-words.sort(key=lambda x: (len(x)))
-
-queue = deque(words)
-# print(words)
-splitWords = []
-w = len(words[0])
-stack = []
-while(queue):
-    cur = queue.popleft()
-    if(len(cur) == w):
-        stack.append(cur)
-        w = len(cur)
-    else:
-        splitWords.append(stack)
-        stack = []
-        stack.append(cur)
-        w = len(cur)
-
-# print(stack)
-if(stack):
-    splitWords.append(stack)
-# print(splitWords)
-
-for i in range(len(splitWords)):
-    splitWords[i].sort()
-
-for i in range(len(splitWords)):
-    for j in range(len(splitWords[i])):
-        print(splitWords[i][j])
+words = [input() for _ in range(n)]
+words = list(set(words))
+words.sort(key=lambda x: (len(x), x))
+for w in words:
+    print(w)
